bd8c2d72e00d943f7e578ebf6beb6856e5659ec3
Abdurrahman <Abdurrahmannoori10@gmail.com>
2022-10-10 23:50:45 +0430

    Added Craete action method

--- StudentGuidence/Areas/Admin/Controllers/UniversityController.cs
+++ StudentGuidence/Areas/Admin/Controllers/UniversityController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StudentGuidenc.DataAccess;
+using StudentGuidence.Models;
 
 namespace StudentGuidence.Areas.Admin.Controllers
 {
@@ -19,5 +20,22 @@ namespace StudentGuidence.Areas.Admin.Controllers
         {
             return View(_db.Universities.ToList());
         }
+        //GET Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST Create
+        [HttpPost]
+        public IActionResult Create(University university)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Universities.Add(university);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
