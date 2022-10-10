47ba903d71b1f80af09e1f53346a46b0c91080e4
Abdurrahman <Abdurrahmannoori10@gmail.com>
2022-10-10 22:28:17 +0430

    Added  University Controller and Index Action method.

---
+++ StudentGuidence/Areas/Admin/Controllers/UniversityController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StudentGuidenc.DataAccess;
+
+namespace StudentGuidence.Areas.Admin.Controllers
+{
+    public class UniversityController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public UniversityController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Universities.ToList());
+        }
+    }
+}
